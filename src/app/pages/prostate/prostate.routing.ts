51aad706ad5625803ba669230607ab44070502f5
bbabic <bbabic@cs.hr>
2022-04-04 14:19:04 +0200

    prostate finished

--- src/app/pages/prostate/prostate.routing.ts
+++ src/app/pages/prostate/prostate.routing.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
       { path: 'general', component: Prostate },
       { path: 'cancer', component: Prostate },
       { path: 'inflamation', component: Prostate },
-      { path: 'stones', component: Prostate },
+      { path: 'enlargement', component: Prostate },
     ],
   },
 ];
